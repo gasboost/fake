2693acecf90d0f3dff65fbe637b9f3a9c9fad609
大島大河 <oshima.automate.growth@gmail.com>
2026-08-31 09:29:52 +0900

    テストを追加

--- packages/fake-core/src/blob/InMemoryBlob.spec.ts
+++ packages/fake-core/src/blob/InMemoryBlob.spec.ts
@@ -12,12 +12,10 @@ describe("初期化", () => {
 });
 
 describe("文字列をBlobにする", () => {
-  it("文字列をBlobにする、InMemoryBlobが作成される", () => {
+  it("空のInMemoryBlobに文字列を文字コード指定せずにセットすると、UTF-8として正しく取得できる", () => {
     const data = new InMemoryBlob("");
     data.setDataFromString("テストデータ");
-    expect(data.getBytes()).toEqual([
-      -29, -125, -122, -29, -126, -71, -29, -125, -120, -29, -125, -121, -29,
-      -125, -68, -29, -126, -65,
-    ]);
+
+    expect(data.getDataAsString()).toBe("テストデータ");
   });
 });
