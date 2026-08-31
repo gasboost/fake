f3d14254b86151352efc73ba1315ed6d758b739f
大島大河 <oshima.automate.growth@gmail.com>
2026-08-31 09:28:04 +0900

    テストを追加

--- packages/fake-core/src/blob/InMemoryBlob.spec.ts
+++ packages/fake-core/src/blob/InMemoryBlob.spec.ts
@@ -10,3 +10,14 @@ describe("初期化", () => {
     ]);
   });
 });
+
+describe("文字列をBlobにする", () => {
+  it("文字列をBlobにする、InMemoryBlobが作成される", () => {
+    const data = new InMemoryBlob("");
+    data.setDataFromString("テストデータ");
+    expect(data.getBytes()).toEqual([
+      -29, -125, -122, -29, -126, -71, -29, -125, -120, -29, -125, -121, -29,
+      -125, -68, -29, -126, -65,
+    ]);
+  });
+});
